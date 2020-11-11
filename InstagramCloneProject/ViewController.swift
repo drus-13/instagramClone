16df29eeab963fb66c2fffbfe4823c48949a8239
rodzaevsky <xcrucified.priestx@gmail.com>
2020-11-11 23:38:41 +0300

    Initial Commit

---
+++ InstagramCloneProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramCloneProject
+//
+//  Created by Andrey Dolgov on 11.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
